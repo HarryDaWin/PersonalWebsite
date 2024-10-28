25721785ab8f151ac8b8813bdd40f8fbfa8c2157
Harrison Huynh <harrisonhuynh999@gmail.com>
2024-10-28 13:45:49 -0700

    remove styles

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-d
 import About from './components/About';
 import Projects from './components/Projects';
 import Resume from './components/Resume';
+import './App.css';
 
 function App() {
   return (
